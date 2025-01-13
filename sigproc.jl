89215b64bacfdc21aee8e007245d153b51ed358a
Vitaliy <ss.vitaliy88@gmail.com>
2025-01-14 00:00:17 +0300

    Реализованы функции для определения параметров модели сигнала.

--- sigproc.jl
+++ sigproc.jl
@@ -1,4 +1,4 @@
-function findextrems(arr, half_win, trig_level; max_cnt=2)
+function findextrems(arr, trig_level; half_win=4, max_cnt=3)
 	
 	pos = Int[]
 	pos_num = 1
@@ -30,13 +30,41 @@ function findextrems(arr, half_win, trig_level; max_cnt=2)
 	return pos
 end
 
-function freqbyextrems(arr, time_step)
-    hp_num = length(arr)-1 # Number of half-periods
+function periodbyextrems(extrems, time_step)
+    hp_num = length(extrems)-1 # Number of half-periods
 
     #hp_time - Median time of 1 half-period
-    hp_time = sum([arr[i+1]-arr[i] for i in 1:hp_num])/hp_num
+    hp_time = sum([extrems[i+1]-extrems[i] for i in 1:hp_num])/hp_num
 
-    # Return frequency
-    (time_step*2*hp_time)^-1
+    # Return signal period
+    time_step*2*hp_time
 end
 
+function calcattenuation(
+			sig::Vector,
+			extrems::Vector,
+			time_step::Real
+		)::Float64
+
+	t1 = extrems[1]
+	t2 = extrems[2]
+	
+	amp_ratio = abs(sig[t2]/sig[t1])
+	return -1*log(amp_ratio) / ((t2-t1)*time_step)
+end
+
+function getstartpoint(extrems::Vector)::Int
+	half_per_num = length(extrems)-1
+	half_per = sum([extrems[i+1]-extrems[i] for i in 1:half_per_num]) /
+				half_per_num
+	round(Int, extrems[1] - half_per/2, RoundUp)
+end
+
+function calcrefparams(sig, trig_level, time_step)
+	extrems = findextrems(sig, trig_level)
+	period = periodbyextrems(extrems, time_step)
+	atten = calcattenuation(sig, extrems, time_step)
+	start_point = getstartpoint(extrems)
+
+	return (1/period, atten, start_point)
+	end
